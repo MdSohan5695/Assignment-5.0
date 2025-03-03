20e39ee8248ab553f7cff78ee9f5f7dff6b45eb5
Md. Sohan <167677539+MdSohan5695@users.noreply.github.com>
2025-03-03 19:30:13 +0600

    check

--- script/anotherHtml.js
+++ script/anotherHtml.js
@@ -2,5 +2,11 @@ document.getElementById("discover-something").addEventListener("click",function(
     window.location.href="./another.html"
 })
 
-
+document.getElementById("conpleted").addEventListener("click",function(){
+    alert("Board updated successfully")
+   let lowNumber = document.getElementById("low-number").innerText;
+   let updatedLowNumber =lowNumber - 1;
+   document.getElementById("low-number").innerText = updatedLowNumber;
+   
+})
 
--- script/return.js
+++ script/return.js
@@ -2,6 +2,6 @@
 
 
 document.getElementById("back-to-desk").addEventListener("click",function(){
-   window.location.href="../index.html"
+   window.location.href="./index.html"
     
 })
